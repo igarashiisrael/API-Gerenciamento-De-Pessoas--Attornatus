6f35ad4940f259c80afcc179244bd0aef4a62b91
Israel <igarashiisrael@icloud.com>
2023-02-18 15:53:25 -0300

    Classes Model adicionadas.

--- src/main/java/com/igarashiisrael/attornatusapi/models/EnderecoModel.java
+++ src/main/java/com/igarashiisrael/attornatusapi/models/EnderecoModel.java
@@ -1,4 +1,83 @@
 package com.igarashiisrael.attornatusapi.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enderecos")
 public class EnderecoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long enderecoId;
+
+    private String Logradouro;
+
+    private String cep;
+
+    private String numero;
+
+    private String cidade;
+
+    private boolean enderecoPrincipal;
+
+    @ManyToOne
+    @JsonIgnoreProperties("endereco")
+    private PessoaModel pessoa;
+
+    public Long getEnderecoId() {
+        return enderecoId;
+    }
+
+    public void setEnderecoId(Long enderecoId) {
+        this.enderecoId = enderecoId;
+    }
+
+    public String getLogradouro() {
+        return Logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        Logradouro = logradouro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public boolean isEnderecoPrincipal() {
+        return enderecoPrincipal;
+    }
+
+    public void setEnderecoPrincipal(boolean enderecoPrincipal) {
+        this.enderecoPrincipal = enderecoPrincipal;
+    }
+
+    public PessoaModel getPessoa() {
+        return pessoa;
+    }
+
+    public void setPessoa(PessoaModel pessoa) {
+        this.pessoa = pessoa;
+    }
 }
